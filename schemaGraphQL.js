67aa7dc0e0f2ddd6423d4d0a44581e0c84b59e65
Warhammer2345 <sebastien-roques@hotmail.fr>
2019-03-22 18:11:04 +0100

    Add request search list of movies by metascore and with a specific size

--- schemaGraphQL.js
+++ schemaGraphQL.js
@@ -67,6 +67,24 @@ const resolvers = {
         return docs[0];
         //return await collection.aggregate([{$match :{ metascore: { $gte: 70 }} },{$sample : {size : 1}}]);
       }, {'retries': ASYNC_MAX_RETRY});
+    },
+    'SearchMovies': async (obj, args, context) => {
+      
+      const {collection} = context;
+      var limit = args.limit;
+      var _metascore = args.metascore;
+      if(args.metascore==null)
+        limit=5;
+      if(_metascore==null)
+        _metascore=0;
+      console.log(args.limit);
+      return await retry(async () => {
+        const cursor = collection.aggregate([{$match :{  metascore: { $gte: parseInt(_metascore,10)  }} },{$sample : {size : parseInt(limit,10) }}]);
+        const docs = await cursor.toArray();
+        console.log(docs);
+        return docs;
+        //return await collection.aggregate([{$match :{ metascore: { $gte: 70 }} },{$sample : {size : 1}}]);
+      }, {'retries': ASYNC_MAX_RETRY});
     }
   }
 };
